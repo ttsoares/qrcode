8a1084522a4b11f2add031bbb7f2c664eed4c82e
Thomas Soares <ttsoares@orion.ufrgs.br>
2023-11-14 22:13:18 -0300

    After feedback from Grace

--- app/page.js
+++ app/page.js
@@ -3,29 +3,33 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex w-full h-full items-start mt-7 md:mt-0 md:items-center justify-center">
-      <section className="flex flex-col w-[322px] h-[495px] bg-white rounded-3xl">
-        <div className="rounded-3xl p-4">
-          <Link href="https://www.frontendmentor.io/challenges">
-            <Image
-              src="/image-qr-code.png"
-              alt="QRcode"
-              className="rounded-xl"
-              width={576}
-              height={576}
-            />
-          </Link>
-        </div>
-
-        <div className="flex flex-col px-8  mt-1.5 text-center">
-          <h1 className="font-bold text-[1.35rem]/[1.7rem] text-dark_blue mb-4">
-            Improve your front-end skills by building projects
-          </h1>
-          <p className="text-grayish_blue text-base/[1.4rem]">
-            Scan the QR code to visit FrontEnd Mentor and take your coding
-            skills to the next level
-          </p>
-        </div>
+    <main className="flex w-full h-full items-start p-4 md:p-0 mt-7 md:mt-0 md:items-center justify-center ">
+      <section className="flex flex-col justify-center  text-center md:max-w-md pb-7 bg-white rounded-3xl">
+        <Link
+          className="rounded-xl p-4"
+          href="https://www.frontendmentor.io/challenges"
+        >
+          <Image
+            src="/image-qr-code.png"
+            alt="QRcode pointing to www.frontendmentor.io"
+            className="rounded-xl mx-auto"
+            width={285}
+            height={285}
+          />
+        </Link>
+        <h1 className="font-bold text-xl text-dark_blue">
+          Improve your front-end
+        </h1>
+        <h1 className="font-bold text-xl text-dark_blue mb-4">
+          skills by building projects
+        </h1>
+        <p className="text-grayish_blue text-base">
+          Scan the QR code to visit FrontEnd
+        </p>
+        <p className="text-grayish_blue text-base">
+          Mentor and take your coding skills
+        </p>
+        <p className="text-grayish_blue text-base">to the next level</p>
       </section>
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,20 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    screens: {
-      sm: "375px",
-      md: "960px",
-      xl: "1440px",
+    fontSize: {
+      base: "0.938rem",
+      xl: "1.45rem",
     },
     extend: {
       colors: {
         light_gray: "hsl(212, 45%, 89%)",
         grayish_blue: "hsl(220, 15%, 55%)",
         dark_blue: "hsl(218, 44%, 22%)",
+      },
+      screens: {
+        sm: "375px",
+        md: "960px",
+        xl: "1440px",
       },
     },
     plugins: [],
